d57fbd2c9490189cde50bf09feacf11ba9bb7f0a
Sergei <tustanovskiis@gmail.com>
2019-06-11 11:51:25 +0300

    slider

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
 var Images = ['background_series_3.jpg','1547367959_4.jpg','1547368018_3.jpg','1547368009_2.jpg'];
-//alert(Images);
 var buttonLeft = document.getElementById('button_gallery_left');
 var buttonRight = document.getElementById('button_gallery_right');
 buttonLeft.addEventListener('click', function(){
@@ -7,7 +6,7 @@ var img = document.getElementById('man');
 	img.src = Images [i+1];
     i++
     if (i>=3){
-    	i=0
+    	i=-1
     }
 
 });
@@ -16,7 +15,7 @@ buttonRight.addEventListener('click',function (){
 	img.src = Images [i+1];
     i++
     if (i>=3){
-    	i=0
+    	i=-1
     }
 });
-i=0;
+i = -1;
